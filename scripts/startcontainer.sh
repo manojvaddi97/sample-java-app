6d535016ada8dcb05788f0170bae12e54d0f09e1
Manoj Vaddi <141347229+manojvaddi97@users.noreply.github.com>
2024-06-16 19:59:48 -0400

    Update startcontainer.sh
    Signed-off-by: Manoj Vaddi <141347229+manojvaddi97@users.noreply.github.com>

--- scripts/startcontainer.sh
+++ scripts/startcontainer.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull manojvaddi497/sample-java-app:latest
+docker pull manojvaddi497/sample-java-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 manojvaddi497/sample-java-app:latest
+docker run -d -p 5000:5000 manojvaddi497/sample-java-app
