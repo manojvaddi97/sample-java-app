f5bd47d00a546bd6a42fa0348f5e7073db67b352
Manoj Vaddi <141347229+manojvaddi97@users.noreply.github.com>
2024-06-16 20:51:40 -0400

    removed files

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull manojvaddi497/sample-java-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 manojvaddi497/sample-java-app
